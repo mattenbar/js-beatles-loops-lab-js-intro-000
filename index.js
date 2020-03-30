6430381e505221888989d830188106fd8af92dfc
matthew enbar <matthew.enbar@gmail.com>
2020-03-30 16:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (let num = 0 ; num < musicians.length ; num++){
+    array.push(`${musicians[num]} plays ${instruments[num]}`)
+  }
+  return array
+}
+function johnLennonFacts(facts){
+  let i = 0
+  var array = []
+  while (i < facts.length){
+    array[i] = `${facts[i]}!!!`
+    i++
+  } return array
+}
